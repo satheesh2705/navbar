d780333af90b3d0fca77ec8100200258d06d7911
satheesh2705 <mrsatheesh2705@gmail.com>
2024-02-01 18:20:38 +0530

    first

---
+++ script.js
@@ -0,0 +1,22 @@
+  const nav = document.querySelector(".nav")
+  const searchIcon = document.querySelector("#searchIcon")
+  const navOpenBtn = document.querySelector(".navOpenBtn")
+  const navCloseBtn = document.querySelector(".navCloseBtn");
+
+searchIcon.addEventListener("click", () => {
+  nav.classList.toggle("openSearch");
+  nav.classList.remove("openNav");
+  if (nav.classList.contains("openSearch")) {
+    return searchIcon.classList.replace("uil-search", "uil-times");
+  }
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+  nav.classList.remove("openSearch");
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
